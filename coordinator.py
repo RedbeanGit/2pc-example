30c2882e5b762af56840049ed10ba3eb143c412d
RedbeanGit <dubois.julien.mail@gmail.com>
2022-10-12 17:08:17 +0200

    fix: Use correct syntaxe
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- coordinator.py
+++ coordinator.py
@@ -1,5 +1,6 @@
 """ Provides a Server class representing the coordinator. """
 
+import logging
 import socket
 import threading
 
@@ -22,8 +23,7 @@ class Server:
     def accept(self):
         """ Wait and accept connections from clients in a separated thread. """
         while self.started:
-            conn, addr = self.socket.accept()
-            print('New connection from', addr)
+            conn, _ = self.socket.accept()
             self.clients.append(conn)
 
     def start(self):
@@ -39,49 +39,48 @@ class Server:
         self.listen_thread.join()
         self.socket.close()
 
-    def run_transaction(self, query):
-        """ Send a transaction to clients. """
-        responses = []
+    def send(self, msg):
+        """ Send a message to all clients. """
+        print('Send', msg)
         for client in self.clients:
-            client.sendall(b'ready?')
-            data = client.recv(1024)
+            client.sendall(msg.encode())
 
+    def recv(self):
+        """ Receive a message from a client. """
+        datas = set()
+        for client in self.clients:
+            data = client.recv(1024)
             if data:
-                responses.append(data.decode())
+                print('Receive', data.decode())
+                datas.add(data.decode())
             else:
                 self.clients.remove(client)
+        return datas
 
-        if 'no' in responses:
-            print('At least one client is not ready')
-            for client in self.clients:
-                client.sendall(b'abort')
+    def run_transaction(self, query):
+        """ Send a transaction to clients. """
+        print('INITIAL')
+        logging.info('begin_commit')
+        self.send(f'PREPARE {query}')
+
+        print('WAIT')
+
+        if 'VOTE-ABORT' in self.recv():
+            logging.info('abort')
+            self.send('GLOBAL-ABORT')
+            print('ABORT')
         else:
-            print('All clients are ready')
-            responses = []
-            for client in self.clients:
-                client.sendall(query.encode())
-                data = client.recv(1024)
-
-                if data:
-                    responses.append(data.decode())
-                else:
-                    self.clients.remove(client)
-            print('Transaction sent')
-
-            if 'fail' in responses:
-                for client in self.clients:
-                    client.sendall(b'abort')
-                print('Transaction aborted')
-            else:
-                for client in self.clients:
-                    client.sendall(b'commit')
-                print('Transaction committed')
+            logging.info('commit')
+            self.send('GLOBAL-COMMIT')
+            print('COMMIT', query)
+        self.recv()
+        logging.info('end_of_transaction')
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='output-coordinator.log')
     server = Server(HOST, PORT)
     server.start()
-    print('Server started')
     try:
         while True:
             server.run_transaction(input('Enter a query: '))
--- participant.py
+++ participant.py
@@ -1,35 +1,61 @@
 """ Connect a client and wait for transactions. """
 
+import logging
 import socket
 
 HOST = 'localhost'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
+def ask_input(msg, yes, nope):
+    """ Ask for an input. """
+    while True:
+        answer = input(f'{msg} ({yes}/{nope}) ')
+        if answer == yes:
+            return True
+        if answer == nope:
+            return False
+
+
+def receive(conn):
+    data = conn.recv(1024).decode()
+    print('Receive', data)
+    return data
+
+
+def send(conn, msg):
+    print('Send', msg)
+    conn.sendall(msg.encode())
+
+logging.basicConfig(filename='output-participant.log')
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    print('Connected to server')
 
-    data = s.recv(1024)
-    print('Received', repr(data))
+    while True:
+        try:
+            data = receive(s)
 
-    if data.decode() == 'ready?':
-        s.sendall(input('Is this db ready? (yes/no) ').encode())
+            if data.startswith('PREPARE'):
+                request = data.split(' ', 1)[1]
+                print('INITIAL')
 
-    data = s.recv(1024)
-    print('Received', repr(data))
+                if ask_input('Ready to commit?', 'yes', 'no'):
+                    logging.info('ready')
+                    send(s, 'VOTE-COMMIT')
+                else:
+                    logging.info('abort')
+                    send(s, 'VOTE-ABORT')
 
-    if data.decode() == 'abort':
-        print('Canceling')
+            print('READY')
+            data = receive(s)
 
-    else:
-        print('Starting transaction', data.decode())
-        s.sendall(input('Is this transaction successful? (success/fail) ').encode())
-
-        data = s.recv(1024)
-        print('Received', repr(data))
-
-        if data.decode() == 'abort':
-            print('Aborting transaction')
-
-        elif data.decode() == 'commit':
-            print('Committing transaction')
+            if data == 'GLOBAL-ABORT':
+                logging.info('abort')
+                print('ABORT')
+            elif data == 'GLOBAL-COMMIT':
+                logging.info('commit')
+                print('COMMIT', request)
+            send(s, 'ACK')
+        except KeyboardInterrupt:
+            print('Closing connection')
+            break
